505ec44720bfccb67556a8a15581aa393022d03b
Billydwilliams <billywilliamton@gmail.com>
2019-12-06 17:07:36 -0700

    Update PixiApngAndGif.js

--- dev/PixiApngAndGif.js
+++ dev/PixiApngAndGif.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
-	typeof define === 'function' && (define.cmd || define.hjs) ? define(function(require,exports,module){module.exports = factory()}) :
+    typeof define === 'function' && (define.cmd || define.hjs) ? define(function(require,exports,module){module.exports = factory()}) :
     (global.PixiApngAndGif = factory());
 }(this, (function () { 'use strict';
 
